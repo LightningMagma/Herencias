aec2c328b208345d1a2f7e783c2d818921a7b2be
Andrexaame <53450386+Andrexaame@users.noreply.github.com>
2022-05-20 13:12:21 -0500

    Agregar archivos de proyecto.

---
+++ Herencias/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    internal interface Interface1
+    {
+        public void Insertar();
+        public void Actualizar();
+        public void Consultar();
+        public void Borrar();
+
+    }
+
+}
